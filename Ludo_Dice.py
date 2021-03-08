0af9d581575344790f448bba89b4f37c2d1ba8d9
abhisun003 <78296681+abhisun003@users.noreply.github.com>
2021-03-08 17:27:41 +0530

    Add files via upload

---
+++ Ludo_Dice.py
@@ -0,0 +1,45 @@
+import random
+a = 'y'
+
+while a == 'y':
+    n= random.randint(1,6)
+    if n==1:
+        print('|-----------|')
+        print('|           |')
+        print('|     *     |')
+        print('|           |')
+        print('|-----------|')
+    if n == 2:
+        print('|-----------|')
+        print('| *         |')
+        print('|           |')
+        print('|         * |')
+        print('|-----------|')
+    if n == 3:
+        print('|-----------|')
+        print('| *         |')
+        print('|     *     |')
+        print('|         * |')
+        print('|-----------|')
+    if n == 4:
+        print('|-----------|')
+        print('| *       * |')
+        print('|           |')
+        print('| *       * |')
+        print('|-----------|')
+    if n == 5:
+        print('|-----------|')
+        print('| *       * |')
+        print('|     *     |')
+        print('| *       * |')
+        print('|-----------|')
+    if n == 6:
+        print('|-----------|')
+        print('| *   *   * |')
+        print('|           |')
+        print('| *   *   * |')
+        print('|-----------|')
+
+    a = input("press y or Y to roll again and any other key to exit:").lower()
+    print('\n')
+    
